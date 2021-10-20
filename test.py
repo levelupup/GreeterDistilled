aeabb73ff2acec74644ed7135bda2fcabfbeddc1
levelupup <46808911+levelupup@users.noreply.github.com>
2021-10-20 15:40:53 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,12 +7,12 @@ import matplotlib.pyplot as plt
 st.set_page_config(layout='wide')
 df = pd.read_csv('india_telecom_performance_indicators.csv')
 category_a = st.selectbox('Select major classification',df['Category_A'].unique())
-category_b = st.selectbox('Select minor classification',df['Category_B'].unique())
 
-df = df[df['Category_A'].eq(category_a) & df['Category_B'].eq(category_b)]
+
+df = df[df['Category_A'].eq(category_a)]
 df = df.sort_values(by='Date')
 
 fig, ax = plt.subplots()
-ax = sns.lineplot(data=df,x='Date',y='value')
+ax = sns.lineplot(data=df,x='Date',y='value',hue='Category_B')
 plt.xticks(rotation='vertical')
 st.pyplot(fig)
