2b39196bfc8a7e84bff6193f246c9307d2419906
levelupup <46808911+levelupup@users.noreply.github.com>
2021-10-20 15:38:11 +0800

    Update test.py

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ category_a = st.selectbox('Select major classification',df['Category_A'].unique(
 category_b = st.selectbox('Select minor classification',df['Category_B'].unique())
 
 df = df[df['Category_A'].eq(category_a) & df['Category_B'].eq(category_b)]
+df = df.sort_values(by='Date')
 
 fig, ax = plt.subplots()
 ax = sns.lineplot(data=df,x='Date',y='value')
