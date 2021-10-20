e54ed0affdde769bffdc7220d7703db0f7063ed4
levelupup <46808911+levelupup@users.noreply.github.com>
2021-10-20 15:46:04 +0800

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ category_a = st.selectbox('Select major classification',df['Category_A'].unique(
 
 df = df[df['Category_A'].eq(category_a)]
 df = df.sort_values(by='Date')
-df = pd.pivot_table(dd,index=['Category_A','Date'],columns=['Category_B'],values='value')
+df = pd.pivot_table(df,index=['Category_A','Date'],columns=['Category_B'],values='value')
 
 fig, ax = plt.subplots()
 ax = sns.lineplot(data=df,x='Date',y='value')
