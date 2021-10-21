6b62408306aecfaafaa49984b4207b8ede3fd5d6
levelupup <46808911+levelupup@users.noreply.github.com>
2021-10-21 11:59:45 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,15 +4,17 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+st.title('World FDI Watch')
 st.set_page_config(layout='wide')
 df = pd.read_csv('world_fdi.csv')
+df['Economy Label'] = df['Economy Label'].replace({'Korea, Republic of':'South Korea','China, Taiwan Province of':'Taiwan'})
 
-asia_countries = ['China','Korea, Republic of','Malaysia','Philippines','Singapore','Thailand','Viet Nam','India','Indonesia','China, Taiwan Province of']
+asia_economies = ['China','Korea','Malaysia','Philippines','Singapore','Thailand','Viet Nam','India','Indonesia','Taiwan']
 
-economy = st.sidebar.multiselect('Select economies',asia_countries)
+economy = st.sidebar.multiselect('Select economies',asia_economies)
 mode = st.sidebar.selectbox('Select flow or stock',df['Mode Label'].unique())
 direction = st.sidebar.selectbox('Select inward or outward',df['Direction Label'].unique())
-year_values = st.sidebar.slider('select years to watch',1970,2020,(2010,2020))
+year_values = st.sidebar.slider('Select years to watch',1970,2020,(2010,2020))
 
 item = st.selectbox('select items',['US dollars at current prices in millions',
                                      'US dollars at current prices per capita',	
@@ -24,9 +26,9 @@ df = df[df['Economy Label'].isin(economy) & df['Mode Label'].eq(mode) & df['Dire
 df = pd.pivot_table(df,index='Year',columns=['Economy Label'],values=item)
 fig, ax = plt.subplots()
 
+st.title('World FDI Watch')
+st.header(item)
 for eco in economy:
     plt.plot(df[eco])
 plt.legend(economy)
-plt.title('World FDI Watch')
-
 st.pyplot(fig)
